95b055b0e41839941cfc95077b0667be547ce17e
Kaif <mohammad.kaif@contenterra.com>
2025-01-15 11:06:16 +0530

    Remove WeatherForecast controller and model; add DotNetEnv package for environment variable management

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 using Books.Models;
 using Books.Services;
 using Microsoft.EntityFrameworkCore;
-
+using DotNetEnv;
 var builder = WebApplication.CreateBuilder(args);
 
+Env.Load();
 // Add services to the container.
-
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
 builder.Services.AddDbContext<BooksContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DbCon")));
+    options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 /*AddScoped: Creates a new instance of the service once per HTTP request. 
   The same instance is used for all components that need it within that request. 
@@ -35,8 +36,8 @@ app.MapControllers();
 
 app.Run();
 
- /*Remove-Migration
-    /// Drop-Database
-    /// Add-Migration UpdateSchema
-    /// Update-Database
-    /// */
+/*Remove-Migration
+   /// Drop-Database
+   /// Add-Migration UpdateSchema
+   /// Update-Database
+   /// */
